61daf4801378f24451ea846b1cc012d6101723d9
lionful <358863284@qq.com>
2019-08-16 13:23:45 +0800

    Update PanelController.java

--- nsf-demo-stock-viewer/src/main/java/com/netease/cloud/nsf/demo/stock/viewer/web/controller/PanelController.java
+++ nsf-demo-stock-viewer/src/main/java/com/netease/cloud/nsf/demo/stock/viewer/web/controller/PanelController.java
@@ -116,7 +116,7 @@ public class PanelController {
     @GetMapping("/health")
     @ResponseBody
     public String health() {
-    	return "I am good! 20190815 21:30";
+    	return "I am good! 20190815 21:50";
     }
     
     @RequestMapping("/deepInvoke")
